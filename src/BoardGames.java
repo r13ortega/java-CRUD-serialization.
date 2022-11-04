a24d15b90adfbbdc5a92d7b7e746b50a4c3f2a23
Rey <reynaldoortega13@gmail.com>
2022-11-04 16:08:38 -0500

    add attributes to BoardGame Class and create CollectionOfBG Class to create ArrayList

--- src/BoardGames.java
+++ src/BoardGames.java
@@ -1,2 +1,25 @@
 public class BoardGames {
+
+    public String boardGameName;
+
+    public int minNumOfPlayers;
+
+    public int maxNumOfPlayers;
+
+    private boolean originalOwner;
+
+    public BoardGames(String boardGameName, int minNumOfPlayers, int maxNumOfPlayers, boolean originalOwner) {
+        this.boardGameName = boardGameName;
+        this.minNumOfPlayers = minNumOfPlayers;
+        this.maxNumOfPlayers = maxNumOfPlayers;
+        this.originalOwner = originalOwner;
+    }
+
+    public boolean isOriginalOwner() {
+        return originalOwner;
+    }
+
+    public void setOriginalOwner(boolean originalOwner) {
+        this.originalOwner = originalOwner;
+    }
 }
